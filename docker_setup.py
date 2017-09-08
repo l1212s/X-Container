464f3d6088c4df9e391e8c1f074c17fab55bd83c
Shannon Joyner <shannonjoyner@dhcp-rhodes-1310.eduroam.cornell.edu>
2017-09-08 13:34:32 -0400

    docker_setup: OpenStack updates
    I ran the script on a Ubuntu OpenStack instance and made updates based on what failed.
    These updates include installing the necessary dependencies to apt-get for docker. I
    also add arguments so you can specify what process and task you want to run.

--- docker_setup.py
+++ docker_setup.py
@@ -1,14 +1,52 @@
+import argparse
+import os
+import re
 import subprocess
 
+def call(command):
+  print('RUNNING COMMAND: ' + ' '.join(command))
+  subprocess.call(command)
+  print('')
+
+def get_known_packages():
+  output = subprocess.check_output(['dpkg', '--get-selections']).decode('utf-8')
+  lines = output.split('\n')
+  packages = list(map(lambda x: x.split('\t')[0], lines))
+  packages = list(filter(lambda x: len(x) > 0, packages))
+  return packages
+
+def install(package, known_packages):
+  if package in known_packages:
+    print(package + " has already been installed")
+  else:
+    call(['apt-get', 'install', '-y', package])
+
 def install_dependencies():
-  subprocess.call(["apt-get", "install", "linux-tools-generic"])
-  subprocess.call(["apt-get", "install", "make"])
-  subprocess.call(["apt-get", "install", "gcc"])
-  subprocess.call(["apt-get", "install", "-y", "docker-ce"])
-  subprocess.call(["git", "clone", "https://github.com/wg/wrk.git"])
+  packages = get_known_packages()
+
+  call(['apt-get', 'update'])
+  install('linux-tools-4.4.0-92-generic', packages)
+  install('make', packages)
+  install('gcc', packages)
+
+  # apt-get may not know about about docker
+  if 'docker-ce' in packages:
+    print('docker-ce has already been installed')
+  else:
+    call(['curl', '-fsSL', 'https://download.docker.com/linux/ubuntu/gpg', '|', 'sudo', 'apt-key', 'add', '-'])
+    call(['add-apt-repository', '"deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"'])
+    call(['apt-get', 'update'])
+    call(['apt-get', 'install', '-y', 'docker-ce'])
+
+  # idk if there's a bette way to do this
+  if not os.path.exists("wrk"):
+    call(['git', 'clone', 'https://github.com/wg/wrk.git'])
+    call(['cd', 'wrk'])
+    call(['make', '-j4'])
+    call(['cd', '..'])
 
 def setup_nginx_configuration(configuration_file_path):
-  configuration = """
+  configuration = '''
 user  nginx;
 worker_processes  1;
 
@@ -29,46 +67,63 @@ http {
 
     include /etc/nginx/conf.d/*.conf;
 }
-"""
-  f = open(configuration_file_path, "w")
+'''
+  f = open(configuration_file_path, 'w')
   f.write(configuration)
   f.close
 
+def docker_port():
+  output = subprocess.check_output(['docker', 'ps']).decode('utf-8')
+  results = re.findall('0.0.0.0:([0-9]+)->80/tcp', output)
+  if len(results) == 0:
+    return None
+  else:
+    return results[0]
+
 def setup_nginx_container():
-  configuration_file_path = "/dev/nginx.conf"
+  configuration_file_path = '/dev/nginx.conf'
   setup_nginx_configuration(configuration_file_path)
 
-  subprocess.call([
-    "docker", "run", "--name", "nginx_container",
-    "-P", "-v", configuration_file_path + ":/etc/nginx/nginx.conf:ro",
-    "-d", "nginx"
-  ])
-
-  output = subprocess.check_output(["docker", "ps"])
-  results = re.findall("0.0.0.0:([0-9]+)->80/tcp", output)
-  port = results[0]
+  port = docker_port()
+  if port == None:
+    call([
+      'docker', 'run', '--name', 'nginx_container',
+      '-P', '-v', configuration_file_path + ':/etc/nginx/nginx.conf:ro',
+      '-d', 'nginx'
+    ])
+    port = docker_port()
+ 
+  print("NGINX running on port " + port)
   return port
 
 def run_nginx_benchmark(num_connections, num_threads, duration, port, measure_performance):
   command = [
-    "./wrk",
-    "-t" + num_threads,
-    "-c" + num_connections,
-    "-d" + duration + "s",
-    "http://localhost:" + port + "/index.html"
+    "wrk/wrk",
+    "-t{:d}".format(num_threads),
+    "-c{:d}".format(num_connections),
+    "-d{:d}s".format(duration),
+    "http://localhost:{:s}/index.html".format(port)
   ]
   if measure_performance:
-    command = command + ["perf", "stat"]
-  subprocess.call(command)
+    command = ['perf', 'stat'] + command
+  call(command)
 
-def setup_docker():
+def setup_docker(args):
   install_dependencies()
-  nginx_port = setup_nginx_container()
-  return nginx_port
+  if args.process == "nginx":
+    port = setup_nginx_container()
+  return port
 
-def run_docker_benchmarks(nginx_port, measure_performance):
-  run_nginx_benchmark(400, 12, 10, nginx_port, measure_performance)
+def run_docker_benchmarks(args, port, measure_performance):
+  if args.process == "nginx":
+    run_nginx_benchmark(400, 12, 10, nginx_port, measure_performance)
 
 if __name__ == '__main__':
-  nginx_port = setup_docker()
-  run_docker_benchmarks(nginx_port, true)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-p', '--process', required=True, help='Indicate which process to run on docker (NGINX, Spark, etc)')
+  parser.add_argument('-t', '--task', required=True, help='What task to run (benchmark, attack)')
+  args = parser.parse_args()
+
+  port = setup_docker(args)
+  if args.task == "benchmark":
+    run_docker_benchmarks(args, port, True)
--- docker_setup.sh
+++ docker_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt install python3
-python docker_setup.py
+python3 docker_setup.py

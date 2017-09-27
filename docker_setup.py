fecc92c7db101ab39921257dc0b672939579bd5d
Zhiming Shen <zshen@cs.cornell.edu>
2017-09-27 13:11:01 -0400

    nginx x-container setup

--- docker_setup.py
+++ docker_setup.py
@@ -10,23 +10,26 @@ NGINX_CONTAINER_PORT = 80
 MEMCACHED_CONTAINER_NAME = "memcached_container"
 MEMCACHED_MACHINE_PORT = 11101
 MEMCACHED_CONTAINER_PORT = 11211
+DOCKER_INSPECT_FILTER = "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"
+XCONTAINER_INSPECT_FILTER = "{{.NetworkSettings.IPAddress}}"
 
 #################################################################################################
 # Common functionality
 #################################################################################################
 
-def call(command):
-  print('RUNNING COMMAND: ' + ' '.join(command))
-  subprocess.call(command)
-  print('')
-
 def shell_call(command):
   print('RUNNING COMMAND: ' + command)
-  subprocess.call(command, shell=True)
+  subprocess.Popen(command, shell=True)
   print('')
 
+def shell_output(command):
+  print('RUNNING COMMAND: ' + command)
+  output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).communicate()[0]
+  print('')
+  return output
+
 def get_known_packages():
-  output = subprocess.check_output(['dpkg', '--get-selections']).decode('utf-8')
+  output = shell_output('dpkg --get-selections').decode('utf-8')
   lines = output.split('\n')
   packages = list(map(lambda x: x.split('\t')[0], lines))
   packages = list(filter(lambda x: len(x) > 0, packages))
@@ -36,13 +39,16 @@ def install(package, known_packages):
   if package in known_packages:
     print(package + " has already been installed")
   else:
-    call(['apt-get', 'install', '-y', package])
+    shell_call('apt-get install -y {:s}'.format(package))
 
 def install_common_dependencies(packages):
   shell_call('apt-get update')
   install('linux-tools-4.4.0-92-generic', packages)
   install('make', packages)
   install('gcc', packages)
+
+def get_ip_address(name):
+  return shell_output("/sbin/ifconfig {0:s} | grep 'inet addr:' | cut -d: -f2 | awk '{{ print $1 }}'".format(name)).strip()
 
 def setup_nginx_configuration(configuration_file_path):
   configuration = '''
@@ -116,6 +122,60 @@ def destroy_container(args):
   else:
     raise "destroy_container: Not implemented"
 
+def setup(args):
+  if args.container == "docker":
+    port = setup_docker(args)
+  elif args.container == "linux":
+    setup_linux(args) 
+  elif args.container == "xcontainer":
+    setup_xcontainer(args)
+  else:
+    raise "setup: Not implemented for container " + args.container
+
+def setup_port_forwarding(machine_ip, machine_port, container_ip, container_port, bridge_ip):
+  shell_call('iptables -I FORWARD -p tcp -d {0:s} -j ACCEPT'.format(container_ip))
+  linux_sleep(1)
+  shell_call('iptables -I FORWARD -p tcp -s {0:s} -j ACCEPT'.format(container_ip))
+  linux_sleep(1)
+  shell_call('iptables -I INPUT -m state --state NEW -p tcp -m multiport --dport {0:d} -s 0.0.0.0/0 -j ACCEPT'.format(machine_port))
+  linux_sleep(1)
+  shell_call('iptables -t nat -I PREROUTING --dst {0:s} -p tcp --dport {1:d} -j DNAT --to-destination {2:s}:{3:d}'.format(machine_ip, machine_port, container_ip, container_port))
+  linux_sleep(1)
+  shell_call('iptables -t nat -I POSTROUTING -p tcp --dst {0:s} --dport {1:d} -j SNAT --to-source {2:s}'.format(container_ip, container_port, bridge_ip))
+  linux_sleep(1)
+  shell_call('iptables -t nat -I OUTPUT --dst {0:s} -p tcp --dport {1:d} -j DNAT --to-destination {2:s}:{3:d}'.format(machine_ip, machine_port, container_ip, container_port))
+  linux_sleep(1)
+
+
+#################################################################################################
+# X-container specific specific
+#################################################################################################
+def generate_xcontainer_ip(bridge_ip):
+  parts = bridge_ip.split(".")
+  last = int(parts[-1])
+  new_last = (last + 1) % 255
+  parts[-1] = str(new_last)
+  return ".".join(parts)
+
+def setup_xcontainer_nginx_container():
+  setup_docker_nginx_container(XCONTAINER_INSPECT_FILTER)
+  docker_id = shell_output('docker inspect --format="{{{{.Id}}}}" {0:s}'.format(NGINX_CONTAINER_NAME)).strip()
+  bridge_ip = get_ip_address('xenbr0')
+  xcontainer_ip = generate_xcontainer_ip(bridge_ip)
+
+  shell_call('docker stop {0:s}'.format(NGINX_CONTAINER_NAME))
+  path = os.getcwd()
+  os.chdir('/root/experiments/native/compute06/docker')
+  machine_ip = get_ip_address('em1')
+  setup_port_forwarding(machine_ip, NGINX_MACHINE_PORT, xcontainer_ip, NGINX_CONTAINER_PORT, bridge_ip)
+  print 'Setup NGINX X-Container on {:s}:{:d}'.format(machine_ip, NGINX_MACHINE_PORT)
+  print 'X-Container will take over this terminal....'
+  shell_call('python run.py --id {0:s} --ip {1:s} --hvm --name {2:s}'.format(docker_id, xcontainer_ip, NGINX_CONTAINER_NAME))
+
+def setup_xcontainer(args):
+  if args.process == "nginx":
+    setup_xcontainer_nginx_container()
+
 #################################################################################################
 # Docker specific
 #################################################################################################
@@ -159,17 +219,18 @@ def nginx_docker_port():
 def memcached_docker_port():
   return docker_port(MEMCACHED_CONTAINER_NAME, '([0-9]+)/tcp -> 0.0.0.0:([0-9]+)')
 
-def setup_docker_nginx_container():
+def setup_docker_nginx_container(docker_filter):
   configuration_file_path = '/dev/nginx.conf'
   setup_nginx_configuration(configuration_file_path)
 
-  address = docker_ip(NGINX_CONTAINER_NAME)
+  address = docker_ip(NGINX_CONTAINER_NAME, docker_filter)
   if address == None:
-    shell_call('docker run --name {:s} -P -v {:s}:/etc/nginx/nginx.conf:ro -d nginx'.format(NGINX_CONTAINER_NAME, configuration_file_path))
-    address = docker_ip(NGINX_CONTAINER_NAME)
+    shell_call('docker run --name {0:s} -P -v {1:s}:/etc/nginx/nginx.conf:ro -d nginx'.format(NGINX_CONTAINER_NAME, configuration_file_path))
+    linux_sleep(5)
+    address = docker_ip(NGINX_CONTAINER_NAME, docker_filter)
  
   ports = nginx_docker_port()
-  print("NGINX running on global port {:s}, container address {:s} port {:s}".format(ports[0], address, ports[1]))
+  print("NGINX running on global port {0:s}, container address {1:s} port {2:s}".format(ports[0], address, ports[1]))
   return ports
 
 def setup_docker_memcached_container():
@@ -188,7 +249,7 @@ def setup_docker_memcached_container():
 
 def docker_port(name, regex):
   try:
-    output = subprocess.check_output('docker port {:s}'.format(name), shell=True).decode('utf-8')
+    output = shell_output('docker port {0:s}'.format(name))
     results = re.findall(regex, output)
     if len(results) == 0:
       return None
@@ -197,17 +258,20 @@ def docker_port(name, regex):
   except subprocess.CalledProcessError as e:
     return None
 
-def docker_ip(name):
+def docker_ip(name, docker_filter):
   try:
-    output = subprocess.check_output("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' " + name, shell=True)
-    return output.decode('utf-8').strip()
+    output = shell_output("docker inspect -f '{0:s}' {1:s}".format(docker_filter, name))
+    output = output.strip()
+    if output == "":
+      return None
+    return output
   except subprocess.CalledProcessError as e:
     return None
 
 def setup_docker(args):
   install_docker_dependencies()
   if args.process == "nginx":
-    port = setup_docker_nginx_container()
+    port = setup_docker_nginx_container(DOCKER_INSPECT_FILTER)
   elif args.process == "memcached":
     port = setup_docker_memcached_container()
   return port
@@ -234,14 +298,9 @@ def install_linux_dependencies():
 def linux_container_execute_command(name, command):
   shell_call('lxc-attach --name ' + name + ' -- /bin/sh -c "' + command + '"')
 
-def setup_port_forwarding(machine_port, container_ip, container_port):
-  print("machine port", machine_port, "container ip", container_ip, "container port", container_port)
-  shell_call('iptables -t nat -A PREROUTING -p tcp -i eth0 --dport {:d} -j DNAT --to-destination {:s}:{:d}'.format(machine_port, container_ip, container_port))
-  shell_call('iptables -t nat -A POSTROUTING -p tcp -d {:s} --dport {:d} -j MASQUERADE'.format(container_ip, container_port))
-
 def get_linux_container_ip(name):
   try:
-    output = subprocess.check_output('lxc-info -n {:s} -iH'.format(name), shell=True)
+    output = shell_output('lxc-info -n {:s} -iH'.format(name))
     return output.decode('utf-8').strip()
   except subprocess.CalledProcessError as e:
     return None
@@ -278,21 +337,20 @@ def start_linux_container(name):
   shell_call('lxc-create --name ' + name + ' -t ubuntu')
   shell_call('lxc-start --name ' + name + ' -d')
 
-def linux_sleep():
-  num_seconds = 5
-  print("Sleeping for {:d} seconds. Linux container network setup is slow....".format(num_seconds))
+def linux_sleep(num_seconds):
+  print("Sleeping for {0:d} seconds. Linux container network setup is slow....".format(num_seconds))
   time.sleep(num_seconds)
 
 def setup_linux_nginx_container():
   start_linux_container(NGINX_CONTAINER_NAME)
-  linux_sleep()
+  linux_sleep(5)
   linux_container_execute_command(NGINX_CONTAINER_NAME, 'sudo apt-get update')
   linux_container_execute_command(NGINX_CONTAINER_NAME, 'sudo apt-get install -y nginx') 
   linux_container_execute_command(NGINX_CONTAINER_NAME, 'systemctl status nginx')
 
 def setup_linux_memcached_container():
   start_linux_container(MEMCACHED_CONTAINER_NAME)
-  linux_sleep()
+  linux_sleep(5)
   linux_container_execute_command(MEMCACHED_CONTAINER_NAME, 'sudo apt-get update')
   linux_container_execute_command(MEMCACHED_CONTAINER_NAME, 'sudo apt-get install -y memcached')
   linux_container_execute_command(MEMCACHED_CONTAINER_NAME, 'memcached -u root &')
@@ -328,7 +386,5 @@ if __name__ == '__main__':
     run_benchmarks(args) 
   elif args.destroy:
     destroy_container(args)
-  elif args.container == "docker":
-    port = setup_docker(args)
-  elif args.container == "linux":
-    setup_linux(args) 
+  else:
+    setup(args) 

e7d95ae686313d6cb35cb0daf3e07b13c6134a58
root <root@en-cs-xcloud01.coecis.cornell.edu>
2017-11-03 23:49:40 -0400

    plot the graph

--- graph.py
+++ graph.py
@@ -1,5 +1,8 @@
-import plotly.plotly as py
+import argparse
+import plotly
 import plotly.graph_objs as go
+import plotly.plotly as py
+import util
 
 CONTAINERS = ['linux', 'docker', 'xcontainer']
 
@@ -15,6 +18,15 @@ METRIC_MAP = {
   'avg_latency': metric_values('Average Latency', 'Throughput', 'Average Latency (usecs)'),
 }
 
+def title(name, process, instances):
+  if process == 'memcached':
+    plural = ""
+    if args.instances > 1:
+      plural = "s"
+    return '{0:s} ({1:d} concurrent instance{2:s})'.format(mm['name'], args.instances, plural)
+  else:
+    return name   
+
 def parse_arguments():
   parser = argparse.ArgumentParser()
   parser.add_argument('-d', '--docker', required=True, help='Docker folder to use')
@@ -22,13 +34,16 @@ def parse_arguments():
   parser.add_argument('-m', '--metric', required=True, help='Metric to graph')
   parser.add_argument('-p', '--process', required=True, help='Application to graph')
   parser.add_argument('-x', '--xcontainer', required=True, help='X-Container folder to use')
+  parser.add_argument('-i', '--instances', type=int, default=1, help='Number of concurrent instances of benchmark running')
   args = parser.parse_args()
   return args
 
 def create_graph(args):
+  plotly.tools.set_credentials_file(username='saj9191', api_key='4VpEqUTtpT2fNnTWmXmZ')
   mm = METRIC_MAP[args.metric]
   data = []
   folders = {}
+
   for container in CONTAINERS:
     date = getattr(args, container)
     folder = util.instance_folder(util.container_folder(args.process, container), date)
@@ -40,16 +55,19 @@ def create_graph(args):
       [x,y] = line.split(',')
       xs.append(x)
       ys.append(y)
+
     trace = go.Scatter(
       x = xs,
       y = ys,
       mode = 'lines+markers',
       name = '{0:s} ({1:s})'.format(container, date)
     )
-    date.append(trace)
-  layout = dict(title = mm['name'], xaxis = dict(title = mm['x-axis']), yaxis = dict(title = mm['yaxis']))
+    data.append(trace)
+
+  layout = dict(title = title(mm['name'], args.process, args.instance), xaxis = dict(title = mm['x-axis']), yaxis = dict(title = mm['y-axis']))
   fig = dict(data=data, layout=layout)
-  py.iplot(fig, filename='test') # TODO: Change to a better name
+  filename = '{0:s}-{1:s}-x{2:s}-d{3:s}-l{4:s}'.format(args.process, args.metric, args.xcontainer, args.docker, args.linux)
+  py.iplot(fig, filename=filename)
 
 def main():
   args = parse_arguments()

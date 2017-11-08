a2d89a93aa9fc38fcecfc787c8272761532afcf8
root <root@en-cs-xcloud01.coecis.cornell.edu>
2017-11-08 17:12:56 -0500

    Add ability to specify arbitrary amount of data we want to graph

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import plotly
 import plotly.graph_objs as go
 import plotly.plotly as py
@@ -31,43 +32,52 @@ def title(name, process, instances, benchmark):
   else:
     return n 
 
-def parse_arguments():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-t', '--test', help='Type of test (bare, CPU)')
-  parser.add_argument('-d', '--docker', required=True, help='Docker folder to use')
-  parser.add_argument('-l', '--linux', required=True, help='Linux folder to use')
-  parser.add_argument('-m', '--metric', required=True, help='Metric to graph')
-  parser.add_argument('-p', '--process', required=True, help='Application to graph')
-  parser.add_argument('-x', '--xcontainer', required=True, help='X-Container folder to use')
-  parser.add_argument('-i', '--instances', type=int, default=1, help='Number of concurrent instances of benchmark running')
-  args = parser.parse_args()
-  util.check_benchmark(args)
+def fetch_points(process, container, date, metric):
+  folder = util.instance_folder(util.container_folder(process, container), date)
+  f = open('{0:s}/{1:s}.csv'.format(folder, metric))
+  points = []
+  for line in f.readlines():
+    line = line.strip()
+    [x,y] = line.split(',')
+    points.append((x,y))
+  points = sorted(points, key=lambda point: point[0])
+  return points
 
-  return args
+def create_json_graph(args):
+  mm = METRIC_MAP[args.metric]
+  data = []
+  with open('json/{0:s}'.format(args.json)) as f:
+    traces = json.load(f)
+  for trace in traces['traces']:
+    points = fetch_points(trace['process'], trace['container'], trace['date'], args.metric)
+    
+    xs = map(lambda p: p[0], points)
+    ys = map(lambda p: p[1], points)
 
-def create_graph(args):
-  plotly.tools.set_credentials_file(username='saj9191', api_key='4VpEqUTtpT2fNnTWmXmZ')
+    trace = go.Scatter(
+      x = xs,
+      y = ys,
+      mode = mm['mode'],
+      name = trace['name'],
+    )
+    data.append(trace)
+
+  layout = dict(title = '{0:s} ({1:s})'.format(traces['title'], args.metric), xaxis = dict(title = mm['x-axis']), yaxis = dict(title = mm['y-axis']))
+  fig = dict(data=data, layout=layout)
+  filename = args.json
+  py.plot(fig, filename=filename)
+
+def create_container_graph(args):
   mm = METRIC_MAP[args.metric]
   data = []
   folders = {}
 
   for container in CONTAINERS:
     date = getattr(args, container)
-    folder = util.instance_folder(util.container_folder(args.process, container), date)
-    f = open('{0:s}/{1:s}.csv'.format(folder, args.metric))
-    points = []
-    for line in f.readlines():
-      line = line.strip()
-      [x,y] = line.split(',')
-      points.append((x,y))
-
-    points = sorted(points, key=lambda point: point[0])
+    points = fetch_points(args.process, container, date, args.metric)
 
     xs = map(lambda p: p[0], points)
     ys = map(lambda p: p[1], points)
-    print(container)
-    print(xs)
-    print(ys)
 
     trace = go.Scatter(
       x = xs,
@@ -82,14 +92,42 @@ def create_graph(args):
   filename = '{0:s}-{1:s}-t-{2:s}-i{3:d}'.format(args.process, args.metric, args.test, args.instances)
   py.plot(fig, filename=filename)
 
+def parse_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-t', '--test', help='Type of test (bare, CPU)')
+  parser.add_argument('-d', '--docker', help='Docker folder to use')
+  parser.add_argument('-j', '--json', help='JSON file to load')
+  parser.add_argument('-l', '--linux', help='Linux folder to use')
+  parser.add_argument('-m', '--metric', required=True, help='Metric to graph')
+  parser.add_argument('-p', '--process', help='Application to graph')
+  parser.add_argument('-x', '--xcontainer', help='X-Container folder to use')
+  parser.add_argument('-i', '--instances', type=int, default=1, help='Number of concurrent instances of benchmark running')
+  args = parser.parse_args()
+
+  if args.json is None:
+    if args.linux is None or args.docker is None or args.xcontainer is None:
+      raise Exception('Need to specify folder for linux, docker and xcontainer')
+    if args.process is None:
+      raise Exception('Need to specify process being graphed')
+    util.check_benchmark(args)
+
+  return args
+
 def main():
   args = parse_arguments()
+  plotly.tools.set_credentials_file(username='saj9191', api_key='4VpEqUTtpT2fNnTWmXmZ')
   if args.metric == 'all':
     for metric in METRIC_MAP.keys():
-      args.metric = metric 
-      create_graph(args)
+      args.metric = metric
+      if args.json:
+  	create_json_graph(args)
+      else:
+        create_container_graph(args)
   else:
-    create_graph(args)
+    if args.json:	
+      create_json_graph(args)
+    else:
+      create_container_graph(args)
 
 if __name__ == '__main__':
   main()

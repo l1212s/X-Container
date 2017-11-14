e5bfe888a04525664148c019eba5c1d654cc9bc1
Zhiming Shen <zshen@cs.cornell.edu>
2017-11-14 13:39:45 -0500

    I can't get same container to work for xcontainer.

--- container.py
+++ container.py
@@ -23,6 +23,7 @@ class Container(object):
     self.application = application
     self.container = container
     self.name = name
+    print("processor_type", processor_type)
     self.processor = util.cpu(processor_type)
     self.mem = util.memory(processor_type)
 
@@ -152,6 +153,9 @@ class XContainer(DockerContainer):
     self.ip_offset = ip_offset
 
   def xconfig(self):
+    nodeId = 0
+    if self.processor > 12:
+      nodeId = 1
     return """
 builder='hvm'
 serial='pty'
@@ -177,8 +181,8 @@ on_reboot = 'restart'
 on_crash = 'preserve'
 #bootloader= 'pygrub'
 device_model_version = "qemu-xen"
-cpus="{0:d}"
-    """.format(self.processor)
+pool="Pool-node{0:d}"
+    """.format(nodeId)
 
   def create_xconfig(self):
     filename = '/root/experiments/native/compute06/docker/docker_hvm.cfg'
@@ -220,6 +224,112 @@ cpus="{0:d}"
     util.tmux_command(self.tmux_name, 'cd /root/experiments/native/compute06/docker')
     util.tmux_command(self.tmux_name, 'python run.py --id {0:s} --ip {1:s} --hvm --name {2:s} --cpu=1'.format(DockerContainer.id(self), self.ip(), self.name))
     time.sleep(10)
+
+
+class BenchmarkContainer(Container):
+  def __init__(self, metric, intensity, name, container, application, processor):
+    Container.__init__(self, name, container, application, processor)
+    self.metric = metric
+    self.intensity = intensity
+    self.duration = 7200
+    self.tmux_name = 'benchmark'
+
+  def start(self):
+    util.shell_call('tmux new -s {0:s} -d'.format(self.tmux_name))
+
+  def benchmark_makefile(self):
+    return '''#.ONESHELL:
+default:
+\\tcd src;\\\\\\\\
+\\tg++ -o memCap memCap.c -lrt;\\\\\\\\
+\\tg++ -o memBw memBw.c;\\\\
+\\tg++ -fopenmp -o cpu cpu.cpp -lpthread -lgomp;\\\\
+\\tg++ -o cpu2 cpu2.cpp;\\\\
+\\tg++ -o l1i l1i.c -lrt;\\\\
+\\tg++ -o l1d l1d.c -lrt;\\\\
+\\tg++ -o l3 l3.c -lrt;\\\\
+\\tg++ -o l2 l2.c -lrt;\\\\
+
+memCap:
+\\tcd src;\\\\
+\\tg++ -o memCap memCap.c -lrt;\\\\
+
+memBw:
+\\tcd src;\\\\
+\\tg++ -o memBw memBw.c;\\\\
+
+cpu:
+\\tcd src;\\\\
+\\tg++ -fopenmp -o cpu cpu.cpp -lpthread -lgomp;\\\\
+
+cpu2:
+\\tcd src;\\\\
+\\tg++ -o cpu2 cpu2.cpp;\\\\
+
+l1i:
+\\tcd src;\\\\
+\\tg++ -o l1i l1i.c -lrt;\\\\
+
+l1d:
+\\tcd src;\\\\
+\\tg++ -o l1d l1d.c -lrt;\\\\
+
+l2:
+\\tcd src;\\\\
+\\tg++ -o l2 l2.c -lrt;\\\\
+'''
+
+  def setup(self, useYum=False):
+    command = 'apt-get'
+    install = 'apt-get install -y'
+    if useYum:
+      command = 'yum'
+      install = 'yum -y install'
+#    util.tmux_command(self.tmux_name, '{0:s} update'.format(install))
+#    time.sleep(5)
+#    util.tmux_command(self.tmux_name, '{0:s} git'.format(install))
+#    time.sleep(40)
+#    util.tmux_command(self.tmux_name, '{0:s} make'.format(install))
+#    time.sleep(5)
+#    util.tmux_command(self.tmux_name, '{0:s} ca-certificates'.format(install))
+#    time.sleep(8)
+#    util.tmux_command(self.tmux_name, '{0:s} g++'.format(install))
+#    time.sleep(100)
+#    util.tmux_command(self.tmux_name, 'cd /home; git clone https://sj677:d057c5e8f966db42a6f467c6029da686fdcf4bb4@github.coecis.cornell.edu/SAIL/XcontainerBolt.git')
+#    time.sleep(8)
+#    util.tmux_command(self.tmux_name, 'cd /home/XcontainerBolt/uBench; truncate -s0 Makefile')
+#    for line in self.benchmark_makefile().split('\n'): 
+#      util.tmux_command(self.tmux_name, "echo -e '{0:s}' >> Makefile".format(line))
+    if False:
+      util.tmux_command(self.tmux_name, 'yum -y install libmpc-devel mpfr-devel gmp-devel')
+      time.sleep(10)
+      util.tmux_command(self.tmux_name, 'curl ftp://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-4.9.2/gcc-4.9.2.tar.bz2 -O')
+      time.sleep(60)
+      util.tmux_command(self.tmux_name, 'tar xvfj gcc-4.9.2.tar.bz2')
+      time.sleep(2)
+      util.tmux_command(self.tmux_name, 'cd gcc-4.9.2')
+      util.tmux_command(self.tmux_name, './configure --disable-multilib --enable-languages=c,c++')
+      time.sleep(5)
+      util.tmux_command(self.tmux_name, 'make -j4')
+      time.sleep(5)
+      util.tmux_command(self.tmux_name, 'make install')
+      time.sleep(600)
+    util.tmux_command(self.tmux_name, 'cd /home/XcontainerBolt/uBench; make')
+    time.sleep(100)
+    self.benchmark()
+
+  def benchmark(self):
+    args = ''
+    if self.metric.startswith('mem'):
+      args = '{0:d} {1:d}'.format(self.duration, self.intensity)
+    elif self.metric.startswith('cpu'):
+      args = '{0:d}'.format(self.duration)
+    else:
+      raise Exception('benchmark - not implemented')
+    util.tmux_command(self.tmux_name, '/home/XcontainerBolt/uBench/src/{0:s} {1:s} &'.format(self.metric, args))
+
+  def destroy(self):
+    util.shell_call('tmux kill-session -t {0:s}'.format(self.tmux_name))
 
 
 class ApplicationContainer(Container):
@@ -295,12 +405,16 @@ def setup_nginx_configuration(configuration_file_path):
   f.close
 
 
-class NginxDockerContainer(DockerContainer, ApplicationContainer, Nginx):
-  def __init__(self):
+class NginxDockerContainer(DockerContainer, ApplicationContainer, Nginx, BenchmarkContainer):
+  def __init__(self, sameContainer=False, metric=None, intensity=None):
     self.config_file = '/dev/nginx.conf'
     self.docker_tmux_name = 'nginx_docker'
     util.shell_call('tmux new -s {0:s} -d'.format(self.docker_tmux_name))
     DockerContainer.__init__(self, 'nginx_container', 'nginx')
+    self.sameContainer = sameContainer
+
+    if sameContainer:
+      BenchmarkContainer.__init__(self, metric, intensity, self.name, 'docker', 'nginx', 'default')
 
   def config(self):
     return ''
@@ -325,9 +439,15 @@ class NginxDockerContainer(DockerContainer, ApplicationContainer, Nginx):
       util.tmux_command(self.docker_tmux_name, "echo '{0:s}' >> etc/nginx/nginx.conf".format(line))
     util.tmux_command(self.docker_tmux_name, 'exit')
 
+  def setup_benchmark(self):
+    util.tmux_command(self.tmux_name, 'docker exec -it {0:s} /bin/bash'.format(self.name))
+    BenchmarkContainer.setup(self, True)
+
   def setup(self):
     DockerContainer.setup(self)
     self.setup_config()
+    if self.sameContainer:
+      self.setup_benchmark()
     ApplicationContainer.setup_port_forwarding(self, self.machine_ip(), self.port, self.ip(), self.port, self.bridge_ip())
     ApplicationContainer.benchmark_message(self)
 
@@ -345,17 +465,27 @@ class MemcachedXContainer(XContainer, MemcachedDockerContainer):
     MemcachedDockerContainer.setup_port_forwarding(self, self.machine_ip(), self.port, self.ip(), self.port, self.bridge_ip())
     MemcachedDockerContainer.benchmark_message(self)
 
-
 class NginxXContainer(XContainer, NginxDockerContainer):
-  def __init__(self):
-    NginxDockerContainer.__init__(self)
+  def __init__(self, sameContainer=False, metric=None, intensity=None):
+    print("init 1")
+    NginxDockerContainer.__init__(self, sameContainer, metric, intensity)
     XContainer.__init__(self, 'nginx_container', 'nginx', 1)
+    print("init 2")
 
   def setup(self):
+    print("setup 1")
     NginxDockerContainer.setup_config(self)
-    XContainer.setup(self)
+    print("setup 2")
+    if self.sameContainer:
+      NginxDockerContainer.setup_benchmark(self)  
+    print("setup 3")
+    #util.shell_call("service docker restart")
+    #XContainer.setup(self)
     NginxDockerContainer.setup_port_forwarding(self, self.machine_ip(), self.port, self.ip(), self.port, self.bridge_ip())
+    print("setup 4")
     NginxDockerContainer.benchmark_message(self)
+    #NginxDockerContainer.benchmark(self)
+    print("setup 5")
 
   def destroy(self):
     XContainer.destroy(self)
@@ -376,47 +506,6 @@ class MemcachedLinuxContainer(LinuxContainer, ApplicationContainer, Memcached):
     util.shell_call("lxc-cgroup -n {0:s} cpuset.cpus {1:d}".format(self.name, self.processor))
     ApplicationContainer.setup_port_forwarding(self, self.machine_ip(), self.port, self.ip(), self.port, self.bridge_ip())
     ApplicationContainer.benchmark_message(self)
-
-
-class BenchmarkContainer(Container):
-  def __init__(self, metric, intensity, name, container, application, processor):
-    Container.__init__(self, name, container, application, processor)
-    self.metric = metric
-    self.intensity = intensity
-    self.duration = 7200
-    self.tmux_name = 'benchmark'
-
-  def start(self):
-    util.shell_call('tmux new -s {0:s} -d'.format(self.tmux_name))
-
-  def setup(self):
-    util.tmux_command(self.tmux_name, 'apt-get update')
-    time.sleep(5)
-    util.tmux_command(self.tmux_name, 'apt-get install -y git')
-    time.sleep(40)
-    util.tmux_command(self.tmux_name, 'apt-get install -y make')
-    time.sleep(5)
-    util.tmux_command(self.tmux_name, 'apt-get install -y ca-certificates')
-    time.sleep(8)
-    util.tmux_command(self.tmux_name, 'apt-get install -y g++')
-    time.sleep(100)
-    util.tmux_command(self.tmux_name, 'cd /home; git clone https://sj677:d057c5e8f966db42a6f467c6029da686fdcf4bb4@github.coecis.cornell.edu/SAIL/XcontainerBolt.git')
-    time.sleep(8)
-    util.tmux_command(self.tmux_name, 'cd /home/XcontainerBolt/uBench; make')
-    time.sleep(10)
-
-  def benchmark(self):
-    args = ''
-    if self.metric.startswith('mem'):
-      args = '{0:d} {1:d}'.format(self.duration, self.intensity)
-    elif self.metric.startswith('cpu'):
-      args = '{0:d}'.format(self.duration)
-    else:
-      raise Exception('benchmark - not implemented')
-    util.tmux_command(self.tmux_name, '/home/XcontainerBolt/uBench/src/{0:s} {1:s}'.format(self.metric, args))
-
-  def destroy(self):
-    util.shell_call('tmux kill-session -t {0:s}'.format(self.tmux_name))
 
 
 class BenchmarkLinuxContainer(LinuxContainer, BenchmarkContainer):
@@ -524,17 +613,17 @@ def parse_arguments():
 
 def balance_xcontainer(b, m, p):
   util.shell_call('python /root/x-container/irq-balance.py')
-  util.shell_call('xl cpupool-migrate {0:s} Pool-node0'.format(m.name))
+  #util.shell_call('xl cpupool-migrate {0:s} Pool-node0'.format(m.name))
   util.shell_call('xl vcpu-pin {0:s} 0 {1:d}'.format(m.name, m.processor), True)
   if b is not None:
-    if p == 'logical':
-      util.shell_call('xl cpupool-migrate {0:s} Pool-node1'.format(b.name), True)
-    else:
-      util.shell_call('xl cpupool-migrate {0:s} Pool-node0'.format(b.name), True)
+    #if p == 'logical':
+    #  util.shell_call('xl cpupool-migrate {0:s} Pool-node1'.format(b.name), True)
+    #else:
+      #util.shell_call('xl cpupool-migrate {0:s} Pool-node0'.format(b.name), True)
     util.shell_call('xl vcpu-pin {0:s} 0 {1:d}'.format(b.name, b.processor), True)
 
 
-def create_application_container(args):
+def create_application_container(args, sameContainer=False):
   if args.container == 'linux':
     if args.application == 'memcached':
       m = MemcachedLinuxContainer()
@@ -547,9 +636,12 @@ def create_application_container(args):
       m = NginxDockerContainer()
   elif args.container == 'xcontainer':
     if args.application == 'memcached':
-      m = MemcachedXContainer()
+      m = MemcachedXContainer(sameContainer)
     else:
-      m = NginxXContainer()
+      if sameContainer:
+        m = NginxXContainer(sameContainer, args.metric, args.intensity) 
+      else:
+        m = NginxXContainer(sameContainer)
   else:
     raise Exception("create_application_container: not implemented")
   return m
@@ -573,26 +665,27 @@ def setup_containers(args):
   #if args.container == 'xcontainer':
   #  util.shell_call('xl cpupool-numa-split')
   if 'same-container' in args.test:
-    raise Exception('not implemented yet')
+    m = create_application_container(args, True)
+    b = None
   else:
     m = create_application_container(args)
     b = create_benchmark_container(args)
 
-    if args.destroy:
-      m.destroy()
-      if b is not None:
-        b.destroy()
-    else:
-      m.start()
-      m.setup()
+  if args.destroy:
+    m.destroy()
+    if b is not None:
+      b.destroy()
+  else:
+    m.start()
+    m.setup()
 
-      if b is not None:
-        b.start()
-        b.setup()
-        b.benchmark()
+    if b is not None:
+      b.start()
+      b.setup()
+      b.benchmark()
 
-      #if args.container == 'xcontainer':
-      #  balance_xcontainer(b, m, get_benchmark_processor(args.test))
+    if args.container == 'xcontainer':
+      balance_xcontainer(b, m, get_benchmark_processor(args.test))
 
 
 def main():
